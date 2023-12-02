30069b60b7d0c27c7e2a629b1b48c26d9eb953ae
aksonai <fatjongerra@gmail.com>
2023-12-02 11:18:22 +0100

    [ADD] solution for day 2 part 2

--- day1/part2.py
+++ day1/part2.py
@@ -1,10 +1,10 @@
 # --- Part Two ---
 
-# Your calculation isn't quite right. It looks like some of the digits are 
-# actually spelled out with letters: one, two, three, four, five, six, seven, 
+# Your calculation isn't quite right. It looks like some of the digits are
+# actually spelled out with letters: one, two, three, four, five, six, seven,
 # eight, and nine also count as valid "digits".
 
-# Equipped with this new information, you now need to find the real first and 
+# Equipped with this new information, you now need to find the real first and
 # last digit on each line. For example:
 
 # two1nine
@@ -15,22 +15,22 @@
 # zoneight234
 # 7pqrstsixteen
 
-# In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. 
+# In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76.
 # Adding these together produces 281.
 
 # What is the sum of all of the calibration values?
 
 str_to_digit = {
-    'zero': 0,
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
 }
 
 reversed_str_to_digit = {word[::-1]: digit for word, digit in str_to_digit.items()}
@@ -42,11 +42,11 @@ def find_first_spelled_digit(s: str, reversed=False) -> (int, str | None):
     else:
         digit_dict = str_to_digit
 
-    for j in range(len(s)-3):
+    for j in range(len(s) - 3):
         for word in digit_dict.keys():
-            # Start to check the first 3 characters in the string, since all the spelled 
+            # Start to check the first 3 characters in the string, since all the spelled
             # digits are 3-5 characters long.
-            i = s[:3+j].find(word)
+            i = s[: 3 + j].find(word)
             if i >= 0:
                 return i, digit_dict[word]
     return -1, None
@@ -78,7 +78,7 @@ def get_number(s: str) -> int:
 
 
 sum = 0
-for line in open('input.txt', 'r'):
+for line in open("input.txt", "r"):
     sum += get_number(line.strip())
 
 print(sum)
--- day2/part2.py
+++ day2/part2.py
@@ -49,28 +49,62 @@
 # only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs
 # of those games?
 
-BAG = {"red": 12, "green": 13, "blue": 14}
+# --- Part Two ---
+#
+# The Elf says they've stopped producing snow because they aren't getting any
+# water! He isn't sure why the water stopped; however, he can show you how to
+# get to the water source to check it out for yourself. It's just up ahead!
+#
+# As you continue your walk, the Elf poses a second question: in each game you
+# played, what is the fewest number of cubes of each color that could have been
+# in the bag to make the game possible?
+#
+# Again consider the example games from earlier:
+#
+# Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+# Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+# Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+# Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+# Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+#
+#     In game 1, the game could have been played with as few as 4 red, 2 green,
+#     and 6 blue cubes. If any color had even one fewer cube, the game would have
+#     been impossible.
+#     Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+#     Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+#     Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+#     Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+#
+# The power of a set of cubes is equal to the numbers of red, green, and blue
+# cubes multiplied together. The power of the minimum set of cubes in game 1 is
+# 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these
+# five powers produces the sum 2286.
+#
+# For each game, find the minimum set of cubes that must have been present. What
+# is the sum of the power of these sets?
 
 
-def check_cube_set(cube_set: str) -> bool:
+def check_cube_set(bag: dict, cube_set: str) -> bool:
     cubes = cube_set.split(", ")
     for cube in cubes:
         cube = cube.split(" ")
         cube_count = int(cube[0])
         cube_color = cube[1]
-        if cube_count > BAG[cube_color]:
-            return False
-    return True
+        if cube_count > bag.get(cube_color, 0):
+            bag[cube_color] = cube_count
+    return bag
 
 
 def check_game(line: str) -> int:
+    bag = dict()
     game_info = line.split(": ")
-    game_id = game_info[0].split(" ")[1]
     cube_sets = game_info[1].split("; ")
     for cube_set in cube_sets:
-        if not check_cube_set(cube_set):
-            return 0
-    return int(game_id)
+        bag = check_cube_set(bag, cube_set)
+    power = 1
+    for key in bag.keys():
+        power *= bag[key]
+    return power
 
 
 game_sum = 0
